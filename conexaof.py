f778e6d8b6fcf5929eac627c84613966b6706527
Daniela2319 <frankvt04@gmail.com>
2024-08-20 17:58:16 -0300

    organizando banco de dados

--- conexaof.py
+++ conexaof.py
@@ -3,25 +3,56 @@ import sqlite3
 conexao = sqlite3.connect("bancoF")
 cursor = conexao.cursor()
 
+
 cursor.execute(
-    "CREATE TABLE Autores(autor_id INT, nome VARCHAR(100), nacionalidade VARCHAR(50));"
+    """
+    CREATE TABLE Autores(
+    autor_id INT, 
+    nome VARCHAR(100), 
+    nacionalidade VARCHAR(50));
+    """
 )
 
 cursor.execute(
-    """CREATE TABLE Editoras(editora_id INTEGER PRIMARY KEY AUTOINCREMENT, nome VARCHAR(100) NOT NULL);"""
+    """
+    CREATE TABLE Editoras(
+    editora_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    nome VARCHAR(100) NOT NULL);
+    """
 )
 
 cursor.execute(
-    "CREATE TABLE Livros (livro_id INTEGER PRIMARY KEY AUTOINCREMENT, titulo TEXT NOT NULL, editora_id INTEGER,max_renovacoes INTEGER, FOREIGN KEY (editora_id) REFERENCES Editoras(editora_id));"
+    """
+    CREATE TABLE Livros (
+    livro_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    titulo TEXT NOT NULL,
+    editora_id INTEGER,
+    max_renovacoes INTEGER, 
+    FOREIGN KEY (editora_id) REFERENCES Editoras(editora_id));
+    """
 )
 
 cursor.execute(
-    "CREATE TABLE Exemplares (exemplar_id INTEGER PRIMARY KEY AUTOINCREMENT, livro_id INTEGER, codigo VACHAR(50), disponivel BOOLEAN DEFAULT 1, FOREIGN KEY (livro_id) REFERENCES Livros(livro_id));"
+    """
+    CREATE TABLE Exemplares (
+    exemplar_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    livro_id INTEGER,
+    codigo VACHAR(50),
+    disponivel BOOLEAN DEFAULT 1,
+    FOREIGN KEY (livro_id) REFERENCES Livros(livro_id));
+    """
 )
 
 cursor.execute(
-    "CREATE TABLE Usuarios (usuario_id INTEGER PRIMARY KEY AUTOINCREMENT, nome VARCHAR(100) NOT NULL, telefone VACHAR(15), nacionalidade VARCHAR(50));"
+    """
+    CREATE TABLE Usuarios (
+    usuario_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    nome VARCHAR(100) NOT NULL,
+    telefone VACHAR(15),
+    nacionalidade VARCHAR(50));
+    """
 )
+
 cursor.execute(
     """
 CREATE TABLE Emprestimos (
@@ -52,40 +83,3 @@ CREATE TABLE Livro_Autores (
 
 conexao.commit()
 conexao.close
-
-# cursor.execute(
-#     "INSERT INTO produtos(id, nome, endereco, email) VALUES (1, 'Dani', 'bromelia', 'dani@gmail.com');"
-# )
-# cursor.execute(
-#     "INSERT INTO produtos(id, nome, endereco, email) VALUES (2, 'Frank', 'Por Ali', 'Frank@gmail.com');"
-# )
-# cursor.execute(
-#     "INSERT INTO produtos(id, nome, endereco, email) VALUES (3, 'Valdemar', 'Ali Perto', 'Val@gmail.com');"
-# )
-# cursor.execute(
-#     "INSERT INTO produtos(id, nome, endereco, email) VALUES (4, 'Fabio', 'flor', 'Fabio@gmail.com');"
-# )
-# cursor.execute(
-#     "INSERT INTO produtos(id, nome, endereco, email) VALUES (5, 'Maria', 'rosas', 'Maria@gmail.com');"
-# )
-# cursor.execute("DELETE FROM usuario where id=1") deleta dados inseridos na tabela
-# dado = cursor.execute("SELECT * FROM produtos")
-# ORDER BY DESC
-# dado = cursor.execute(
-#     "SELECT * FROM produtos ORDER BY nome "
-# )  # ordena na ordem alfabetica
-# dado = cursor.execute("SELECT * FROM produtos ORDER BY nome DESC")   ordena descresente
-
-# LIMIT E DISTINCT
-# dado = cursor.execute("SELECT * FROM produtos LIMIT 3")
-
-# GROUP BY E HAVING
-# dado = cursor.execute("SELECT nome FROM produtos GROUP BY nome")
-# dado = cursor.execute("SELECT nome FROM produtos GROUP BY nome HAVING id > 3")
-
-# JOIN´s
-# for produtos in dado:
-#     print(produtos)
-# cursor.execute('UPDATE usuario SET endereco="Minha Rua" WHERE nome="Valdemar"')
-
-# para ocorre tudo certo conexão com banco de dado sqlite tem entra na pasta roda a python conexaoC.py
